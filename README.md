55ad90ab806ea1edf6d4f6e83780c6b690227fd2
Jeff Kellem <github-dist@beyond.dreams.org>
2023-05-28 21:27:36 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,37 @@
+# ASMAC Cornerstone Groups Condition WordPress Plugin
+
+Adds support to use [itthinx Groups](https://github.com/itthinx/groups) group membership in themeco's Cornerstone [conditions and assignments](https://theme.co/docs/conditions-and-assignments).
+
+## Requirements
+
+* WordPress -- tested with WordPress 6.2.2.
+* PHP 8.0.28 -- tested with PHP 8.0.28.
+* themeco [Cornerstone](https://theme.co/docs/cornerstone-overview) builder -- tested with Cornerstone 7.2.5 using Pro theme 6.2.5.
+* itthinx [Groups WordPress plugin](https://wordpress.org/plugins/groups/) -- tested with Groups 2.18.0
+
+## Installation
+
+1. Upload or extract the `asmac-cornerstone-groups-condition` folder to your site's `/wp-content/plugins/` directory. You can also use the *Add new* option found in the *Plugins* menu in WordPress.  
+2. Enable the plugin from the *Plugins* menu in WordPress.
+
+
+## Usage
+
+Once activated, in the Cornerstone [conditions and assignments](https://theme.co/docs/conditions-and-assignments) area, you'll see a new option: `Current User Groups Membership` with a set of options based on the groups configured in the [Groups](https://docs.itthinx.com/document/groups/setup/groups/) WordPress plugin.
+
+![image](./images/pulldown.png)
+
+![image](./images/is-current.png)
+
+Before writing this plugin, we were using [`[groups_member group="groupname"]`](https://docs.itthinx.com/document/groups/shortcodes/groups_member/) and [`[groups_non_member…]`](https://docs.itthinx.com/document/groups/shortcodes/groups_non_member/) Groups shortcodes wrapped around [Raw Content elements](https://theme.co/docs/raw-content) in Cornerstone. With this plugin enabled, now any element can have a condition based on Groups membership.
+
+## Limitations
+
+At the moment, there's no way to preview with various group membership. That might be added later if feasible with Cornerstone's `preview_contexts`.
+
+## Credits
+
+Written by Jeff Kellem for ASMAC (American Society of Music Arrangers and Composers). Instead of writing up notes of why themeco should add support for Groups, the initial code was written, though the [docs for conditions](https://theme.co/docs/cornerstone-developer-guide#conditions) is lacking.
+
+© 2023 Jeff Kellem
+License: BSD-2-CLAUSE
---
+++ asmac-cornerstone-groups-condition.php
@@ -0,0 +1,83 @@
+<?php
+/*
+   Plugin Name: ASMAC Cornerstone Groups Condition
+   Description: Adds itthinx Groups group membership to themeco's Cornerstone conditions
+   Version: 0.5
+   Author: Jeff Kellem
+   Author URI: https://slantedhall.com/
+   License: BSD-2-Clause
+	License URI: http://opensource.org/licenses/BSD-2-Clause
+
+   	Copyright 2023 Jeff Kellem.
+*/
+
+
+class ASMAC_Cornerstone_Groups_Condition {
+
+	protected static $instance;
+
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new ASMAC_Cornerstone_Groups_Condition();
+		}
+ 		return self::$instance;
+	}
+
+	public function setup() {
+		add_filter( 'cs_condition_contexts', [$this, 'condition_contexts'] );
+		// need to change cs_assignment_contexts if need to be different, later
+		// right now, just using user is member of. Similar to built-in global conditions and assignments in Cornerstone.
+		add_filter( 'cs_assignment_contexts', [$this, 'condition_contexts'] );
+		add_filter( 'cs_condition_rule_groups_is_member', [ $this, 'condition_rule_groups_is_member' ], 10, 2 );
+	}
+
+	public function condition_contexts( $contexts ) {
+		$contexts['labels']['groups'] = __( 'Groups', 'groups' );
+		$contexts['controls']['groups'] = $this->assignment_and_condition_contexts_groups();
+
+		return $contexts;
+	}
+
+	public function assignment_and_condition_contexts_groups() {
+		return [
+			[
+				'key'	 => 'groups:is-member',
+				'label'	 => __('Current User Groups Membership', 'groups'),	// different __() FIXME:reword and use different group
+				'toggle' => ['type' => 'boolean'],
+				'criteria' => [
+					'type'	=> 'select',
+					'choices' => $this::get_groups_options()
+				]
+			]
+		];
+	}
+
+	public function get_groups_options() {
+		$groups  = Groups_Group::get_groups(
+			array(
+				'order_by' => 'name',
+				'order'    => 'ASC',
+			)
+		);
+		foreach ( $groups as $group ) {
+			$groups_options[] = array(
+				'value' => $group->group_id,
+				'label' => $group->name ? stripslashes( wp_filter_nohtml_kses( $group->name ) ) : '',
+			);
+		}
+		return $groups_options;
+	}
+
+	public function condition_rule_groups_is_member( $result, $args ) {
+		list($group_id) = $args;
+
+		$is_a_member = false;
+		require_once( ABSPATH . 'wp-includes/pluggable.php' );
+		$is_a_member = Groups_User_Group::read( get_current_user_id() , $group_id );
+
+		return $is_a_member;
+	}
+
+}
+
+ASMAC_Cornerstone_Groups_Condition::instance()->setup();
